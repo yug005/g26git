5a1654ef8f746b437f2b9fa1032cfd00be9dcefb
yug005 <yuga6627@gmail.com>
2025-11-24 15:07:21 +0530

    created model

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase")
+.then(() => {
+    console.log("Connected to MongoDB");
+});
+app.listen(3000, () => {
+    console.log('Server 3000');
+});
---
+++ model/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
