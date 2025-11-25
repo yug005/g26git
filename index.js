f628b8425492bfbcbdee95c510c6cc5109b7614f
yug005 <yuga6627@gmail.com>
2025-11-25 14:27:20 +0530

    blog created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const User = require('./model/user');
 
 const app = express();
 app.use(express.json()); 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.use('/blogs', require('./routes/blogs_routes'));
 mongoose.connect("mongodb://127.0.0.1:27017/mydatabase")
 .then(() => {
     console.log("Connected to MongoDB");
